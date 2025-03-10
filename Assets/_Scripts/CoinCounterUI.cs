20d01a127a8d9a70a13a3933f5b6658f220b1b34
Juan Ablan <ablanjuan1@gmail.com>
2025-03-10 16:49:59 -0700

    Created Easing Functionality for Score

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score) {
+        toUpdate.SetText($"{score}"); //set the needed update score to the according score
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration); //move the score number up
+        //using the container itself
+
+        StartCoroutine(ResetContainerScore(score));
+    }  
+
+    IEnumerator ResetContainerScore(int score) {
+        yield return new WaitForSeconds(duration); //wait for however long duration is
+        //duration = duration of animation
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+}
+
+
