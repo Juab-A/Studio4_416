09cf395c4463bfc5ce910237f88e3d25e50466f4
Juan Ablan <ablanjuan1@gmail.com>
2025-03-11 09:16:46 -0700

    Implemented Setting Menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
@@ -6,12 +7,56 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     [SerializeField] private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private CoinCounterUI coinCounter;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private InputManager inputManager;
+
+    private bool isSettingsMenuActive;
+
+    //Creates public getter for the bool
+    //So that the variable is read only without making it public
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+
+        //Start game with settings menu disabled
+        DisableSettingsMenu();
+    }
+
+    private void ToggleSettingsMenu()
+    {
+        if (isSettingsMenuActive) DisableSettingsMenu();
+        else EnableSettingsMenu();
+    }
+
+    private void EnableSettingsMenu()
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }  
+    public void DisableSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 
     public void IncreaseScore()
--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,6 +6,8 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new();
     public UnityEvent OnJump = new();
     public UnityEvent OnDash = new();
+    public UnityEvent OnSettingsMenu = new();
+
     void Update()
     {
         Vector2 input = Vector2.zero;
@@ -35,5 +37,13 @@ public class InputManager : MonoBehaviour
             OnDash?.Invoke();
         }
         OnMove?.Invoke(input.normalized);
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            OnSettingsMenu?.Invoke();
+        }
+
+        //With that IsSettingMenuActive variable we created in the GameManager
+        //We're able to read the value of the bool at any given point, without changing it
+        if (GameManager.Instance.IsSettingsMenuActive) return;
     }
 }
